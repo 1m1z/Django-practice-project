914fb1cf3b2fdee8a54e6f2dd5db63d3aa79f261
Amirali Golru <golruamirali@gmail.com>
2022-12-03 19:28:56 +0330

    blog models added

--- MercedesBenz/MercedesBenz/settings.py
+++ MercedesBenz/MercedesBenz/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'landingpage.apps.LandingpageConfig',
-
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
--- MercedesBenz/blog/models.py
+++ MercedesBenz/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    class Meta:
+        verbose_name="پست ها"
+        verbose_name_plural="پست ها"
+
+    BlogPic=models.ImageField(upload_to="media/blog" ,verbose_name="عکس پست")
+    BlogName=models.CharField(max_length=32 ,verbose_name="اسم پست")
+    BlogDescription=models.TextField(verbose_name="توضیحات مختصر")
+
+    def __str__(self):
+        return self.BlogPic

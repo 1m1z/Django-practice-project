ddb5c15deef7fa9e08d5f7492733f451bcde23fd
Amirali Golru <golruamirali@gmail.com>
2022-12-08 16:41:18 +0330

    query in url fixed and ready to buildup!

--- MercedesBenz/blog/views.py
+++ MercedesBenz/blog/views.py
@@ -11,11 +11,10 @@ def Bloglistview(request):
     return render(request , "blog/bloglists.html" , context )
 
 def BlogListDetailsVeiw(request,post_id):
-
-    blogdetails = Blog.objects.get(pk = post_id)
+    postmost = Blog.objects.get(pk = post_id)
 
     context={
-        "postdetails":blogdetails
+        "postdetails": postmost
     }
 
     return render(request,"blog/blogdetails.html", context)
